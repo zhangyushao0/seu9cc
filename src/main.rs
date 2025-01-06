ba2d40a0c11e501a0bd513d3566aa3eb6d1451d2
zhangyushao <zhangyushaow@gmail.com>
2025-01-06 10:16:49 +0800

    重构代码

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-pub mod common;
-pub mod lexer;
-pub mod parser;
+extern crate seu9cc;
+
+use seu9cc::gen_ir::gen_ir;
+use seu9cc::irdump::dump_ir;
+use seu9cc::parse::parse;
+use seu9cc::preprocess::Preprocessor;
+use seu9cc::regalloc::alloc_regs;
+use seu9cc::sema::sema;
+use seu9cc::token::tokenize;
+
 use std::env;
 use std::process;
 
 fn usage() -> ! {
-    eprintln!("Usage: seu9tk <file>");
+    eprintln!("Usage: seu9cc [-t] <file>");
     process::exit(1)
 }
 
@@ -15,12 +22,41 @@ fn main() {
         usage();
     }
 
-    let file_name = &args[1];
-    let file_content = std::fs::read_to_string(file_name).unwrap();
-    let mut lexer = lexer::Lexer::new(&file_content, file_name, &file_content);
-    lexer.scan();
-    let tokens = lexer.tokens;
-    for token in tokens {
-        println!("{:?}", token.kind);
+    let mut dump_ir1 = false;
+    let mut dump_tokens = false;
+    let path;
+
+    if args[1] == "-t" {
+        if args.len() < 3 {
+            usage();
+        }
+        dump_tokens = true;
+        path = &args[2];
+    } else {
+        path = &args[1];
+        dump_ir1 = true;
     }
+
+    // Tokenize and parse.
+    let tokens = tokenize(path.clone(), &mut Preprocessor::new());
+
+    if dump_tokens {
+        for token in &tokens {
+            println!(
+                "ty:{:?}, start:{}, end:{}",
+                token.ty, token.start, token.end
+            );
+        }
+        return;
+    }
+
+    let nodes = parse(&tokens);
+    let (nodes, globals) = sema(nodes);
+    let mut fns = gen_ir(nodes);
+
+    if dump_ir1 {
+        dump_ir(&fns);
+    }
+
+    // gen_x86(globals, fns);
 }
